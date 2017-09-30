0a8196c0b47e65075f7e9d65ae183cbe3ee0a38c
epoll <kunqian_jobs@163.com>
2017-09-30 11:21:58 +0800

    Add files via upload

---
+++ Device driver/mutex/test.c
@@ -0,0 +1,41 @@
+#include<stdio.h>
+#include<sys/types.h>
+#include<sys/stat.h>
+#include<fcntl.h>
+#include<unistd.h>
+int main(int argc, const char *argv[])
+{
+	int fd;
+	
+	pid_t pid;
+	fd = open("/dev/demo",O_RDWR);
+	if(fd == -1)
+	{
+		perror("open");
+		return -1;
+	}
+
+	pid = fork();
+
+	if(pid == -1)
+	{
+
+	}
+	else if(pid == 0)//child
+	{
+		printf("*************write1************\n");
+		write(fd,"hello",5);
+		printf("*************write2************\n");
+	}
+	else //
+	{
+		sleep(1);
+		printf("*************read1************\n");
+		char buf[10];
+		read(fd,buf,sizeof(buf));
+		printf("read:%s\n",buf);
+		printf("*************read2************\n");
+
+	}
+	return 0;
+}
